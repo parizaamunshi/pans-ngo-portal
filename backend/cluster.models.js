d5c8bf4211e71fba4586a786b6f13c02cda140c8
rupesh-mr <rupeshmr2005@gmail.com>
2025-07-13 01:01:28 +0530

    cluster schema added

---
+++ backend/cluster.models.js
@@ -0,0 +1,73 @@
+const mongoose = require('mongoose');
+const artisanSchema = new mongoose.Schema({
+    artisan_id: {
+        type: Number,
+        required: true,
+        unique: true,
+
+    },
+    artisan_name:{
+        type: String,
+        required: true
+    },
+    artisan_rating: {
+        type: Number,
+        required: true,
+        default: 5
+    },
+    total_orders: {
+        type: Number,
+        required: true,
+        default:true
+    },
+    total_revenue: {
+        type: Number,
+        required: true,
+        default:true
+    },
+    current_order:{
+        type:Number,
+        required: true,
+        default:0
+    },
+    amount_to_be_paid:{
+        type:Number,
+        required: true,
+        default:0
+    },
+    skills:{
+        type:[String],
+        require:false
+    }
+})
+
+
+const clusterSchema = new mongoose.Schema({
+    cluster_id: {
+        type:Number,
+        required: true,
+        unique: true
+    },
+    leader_id:{
+        type: Number,
+        required: true
+    },
+    cluster_rating:{
+        type: Number,
+        require: true
+    },
+    leader_rating:{
+        type: Number,
+        require: true
+    },
+    order_id:{
+        type: Number,
+        default:None
+    }
+    ,
+    artisans:[artisanSchema]
+
+});
+
+const cluster = mongoose.model('Cluster', clusterSchema);
+module.exports = cluster
